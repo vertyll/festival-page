f65e48c813c3a8295f2ca95c3fd9193c512f879d
Mikołaj <gawrmiko@gmail.com>
2023-10-27 12:06:37 +0200

    navbar jest responsywny

--- componenets/molecules/Navbar.jsx
+++ componenets/molecules/Navbar.jsx
@@ -1,7 +1,8 @@
 import Link from "next/link";
-import { useContext } from "react";
+import { useState, useContext } from "react";
 import styled, { css } from "styled-components";
 import { CartContext } from "../organism/CartContext";
+import IconHamburger from "../atoms/IconHamburger";
 
 const StyledDiv = styled.div`
   max-width: 800px;
@@ -12,7 +13,7 @@ const StyledDiv = styled.div`
 const StyledLink = styled(Link)`
   color: var(--main-maize-color);
   text-decoration: none;
-
+  cursor: pointer;
   svg {
     height: 24px;
   }
@@ -31,27 +32,138 @@ const StyledLink = styled(Link)`
         filter: brightness(0.85);
       }
     `}
+
+  @media (max-width: 768px) {
+    display: block;
+    padding: 10px 20px;
+  }
 `;
 
 const StyledNav = styled.nav`
   display: flex;
   gap: 20px;
   align-items: center;
+
+  @media (max-width: 768px) {
+    display: ${(props) => (props.$open ? "flex" : "none")};
+    flex-direction: column;
+    width: 100%;
+    margin-top: 25px;
+    gap: 10px;
+  }
+`;
+
+const NavButton = styled.button`
+  background-color: transparent;
+  border: none;
+  width: 45px;
+  height: 45px;
+  color: var(--light-color);
+  cursor: pointer;
+
+  @media (min-width: 769px) {
+    display: none;
+  }
+`;
+
+const MobileMenuWrapper = styled.div`
+  display: none;
+
+  @media (max-width: 768px) {
+    display: block;
+    position: fixed;
+    top: 0;
+    right: 0;
+    height: 100vh;
+    width: 300px;
+    background: var(--nav-color);
+    transform: translateX(100%);
+    transition: transform 0.3s ease-in-out;
+    z-index: 2;
+
+    ${(props) =>
+      props.$open &&
+      css`
+        transform: translateX(0);
+      `}
+  }
+`;
+
+const CloseButton = styled.button`
+  position: absolute;
+  top: 20px;
+  right: 20px;
+  border: none;
+  background-color: transparent;
+  border: none;
+  width: 45px;
+  height: 45px;
+  color: var(--light-color);
+  cursor: pointer;
+`;
+
+const CenterDiv = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const PageOverlay = styled.div`
+  display: none;
+
+  @media (max-width: 768px) {
+    ${(props) =>
+      props.$open &&
+      css`
+        display: block;
+        position: fixed;
+        top: 0;
+        left: 0;
+        right: 0;
+        bottom: 0;
+        background: rgba(0, 0, 0, 0.5);
+        z-index: 1;
+      `}
+  }
 `;
 
 export default function Navbar() {
   const { cartProducts } = useContext(CartContext);
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+
+  const toggleMobileMenu = () => {
+    setIsMobileMenuOpen(!isMobileMenuOpen);
+  };
+
   return (
     <StyledDiv>
-      <StyledNav>
-        <StyledLink href={"/products"}>Sklep</StyledLink>
-        <StyledLink $usage="special" href={"/account"}>
-          Konto
-        </StyledLink>
-        <StyledLink $usage="special" href={"/cart"}>
-          Koszyk ({cartProducts.length})
-        </StyledLink>
-      </StyledNav>
+      <CenterDiv>
+        <StyledNav>
+          <StyledLink href="/products">Sklep</StyledLink>
+          <StyledLink href="/account" $usage="special">Konto</StyledLink>
+          <StyledLink href="/cart" $usage="special">
+            Koszyk ({cartProducts.length})
+          </StyledLink>
+        </StyledNav>
+        <NavButton onClick={toggleMobileMenu}>
+          <IconHamburger />
+        </NavButton>
+      </CenterDiv>
+      <PageOverlay $open={isMobileMenuOpen} onClick={toggleMobileMenu} />
+      <MobileMenuWrapper $open={isMobileMenuOpen}>
+        {isMobileMenuOpen && (
+          <CloseButton onClick={toggleMobileMenu}>
+            <IconHamburger />
+          </CloseButton>
+        )}
+
+        <StyledNav $open={isMobileMenuOpen}>
+          <StyledLink href="/products">Sklep</StyledLink>
+          <StyledLink href="/account">Konto</StyledLink>
+          <StyledLink href="/cart">
+            Koszyk ({cartProducts.length})
+          </StyledLink>
+        </StyledNav>
+      </MobileMenuWrapper>
     </StyledDiv>
   );
 }
