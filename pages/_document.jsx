4d949a3b03db566372de4cfb889fa9f01fb8aeac
Mikołaj Gawron <gawrmiko@gmail.com>
2023-11-16 08:36:03 +0100

    dod render stylowania po str ser aby unik FOUC

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -1,13 +1,49 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import Document, { Html, Head, Main, NextScript } from "next/document";
+import { ServerStyleSheet } from "styled-components";
 
-export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  );
+export default class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    // Tworzenie nowego arkusza stylów
+    const sheet = new ServerStyleSheet();
+    const originalRenderPage = ctx.renderPage;
+
+    try {
+      // Zastąpienie renderowania strony, aby dołączyć style
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: (App) => (props) =>
+            sheet.collectStyles(<App {...props} />),
+        });
+
+      // Pobranie początkowych właściwości dokumentu
+      const initialProps = await Document.getInitialProps(ctx);
+
+      // Zwrócenie właściwości wraz ze stylem strony
+      return {
+        ...initialProps,
+        styles: (
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>
+        ),
+      };
+    } finally {
+      // Upewnienie się, że arkusz stylów jest uszczelniony
+      sheet.seal();
+    }
+  }
+
+  // Renderowanie struktury dokumentu HTML
+  render() {
+    return (
+      <Html>
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
