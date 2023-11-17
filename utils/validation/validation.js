ca5c1490a591987aefe72ea8ad22f870dadc4ef7
Mikołaj Gawron <gawrmiko@gmail.com>
2023-11-17 17:53:44 +0100

    refaktoryzacja walidacji

--- utils/validation/validation.js
+++ utils/validation/validation.js
@@ -8,37 +8,21 @@ import {
 } from "./validators";
 
 export const validateFormValues = (values, fieldsToValidate = []) => {
-  let errors = {};
+  const validators = {
+    name: validateName,
+    email: validateEmail,
+    city: validateCity,
+    postalCode: validatePostalCode,
+    streetAddress: validateStreetAddress,
+    country: validateCountry,
+  };
 
-  if (fieldsToValidate.includes("name")) {
-    const nameError = validateName(values.name);
-    if (nameError) errors.name = nameError;
-  }
-
-  if (fieldsToValidate.includes("email")) {
-    const emailError = validateEmail(values.email);
-    if (emailError) errors.email = emailError;
-  }
-
-  if (fieldsToValidate.includes("city")) {
-    const cityError = validateCity(values.city);
-    if (cityError) errors.city = cityError;
-  }
-
-  if (fieldsToValidate.includes("postalCode")) {
-    const postalCodeError = validatePostalCode(values.postalCode);
-    if (postalCodeError) errors.postalCode = postalCodeError;
-  }
-
-  if (fieldsToValidate.includes("streetAddress")) {
-    const streetAddressError = validateStreetAddress(values.streetAddress);
-    if (streetAddressError) errors.streetAddress = streetAddressError;
-  }
-
-  if (fieldsToValidate.includes("country")) {
-    const countryError = validateCountry(values.country);
-    if (countryError) errors.country = countryError;
-  }
-
-  return errors;
+  return fieldsToValidate.reduce((errors, field) => {
+    const validator = validators[field];
+    const error = validator ? validator(values[field]) : null;
+    if (error) {
+      errors[field] = error;
+    }
+    return errors;
+  }, {});
 };
--- utils/validation/validators.js
+++ utils/validation/validators.js
@@ -1,62 +1,52 @@
+const MAX_NAME_LENGTH = 30;
+const MAX_CITY_LENGTH = 50;
+const MAX_STREET_LENGTH = 100;
+const MAX_COUNTRY_LENGTH = 50;
+const EMAIL_PATTERN = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+const isTooLong = (value, maxLength) => value.trim().length > maxLength;
+
 export const validateName = (name) => {
-  if (!name.trim()) {
-    return "Proszę wpisać imię.";
-  } else if (name.trim().length > 30) {
+  if (!name.trim()) return "Proszę wpisać imię.";
+  if (isTooLong(name, MAX_NAME_LENGTH))
     return "Nazwa nie może przekraczać 30 znaków.";
-  }
 
   return null;
 };
 
 export const validateEmail = (email) => {
-  // Jeśli email jest pusty lub składa się tylko z białych znaków
-  if (!email.trim()) {
-    return "Proszę wpisać adres email.";
-  }
-
-  const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-
-  if (!emailPattern.test(email)) {
-    return "Nieprawidłowy format adresu email.";
-  }
+  if (!email.trim()) return "Proszę wpisać adres email.";
+  if (!EMAIL_PATTERN.test(email)) return "Nieprawidłowy format adresu email.";
 
   return null;
 };
 
 export const validateCity = (city) => {
-  if (!city.trim()) {
-    return "Proszę wpisać nazwę miasta.";
-  } else if (city.trim().length > 50) {
+  if (!city.trim()) return "Proszę wpisać nazwę miasta.";
+  if (isTooLong(city, MAX_CITY_LENGTH))
     return "Nazwa miasta nie może przekraczać 50 znaków.";
-  }
 
   return null;
 };
 
 export const validatePostalCode = (postalCode) => {
-  if (!postalCode.trim()) {
-    return "Proszę wpisać kod pocztowy.";
-  }
+  if (!postalCode.trim()) return "Proszę wpisać kod pocztowy.";
 
   return null;
 };
 
 export const validateStreetAddress = (streetAddress) => {
-  if (!streetAddress.trim()) {
-    return "Proszę wpisać adres ulicy.";
-  } else if (streetAddress.trim().length > 100) {
+  if (!streetAddress.trim()) return "Proszę wpisać adres ulicy.";
+  if (isTooLong(streetAddress, MAX_STREET_LENGTH))
     return "Adres ulicy nie może przekraczać 100 znaków.";
-  }
 
   return null;
 };
 
 export const validateCountry = (country) => {
-  if (!country.trim()) {
-    return "Proszę wpisać kraj.";
-  } else if (country.trim().length > 50) {
+  if (!country.trim()) return "Proszę wpisać kraj.";
+  if (isTooLong(country, MAX_COUNTRY_LENGTH))
     return "Nazwa kraju nie może przekraczać 50 znaków.";
-  }
 
   return null;
 };
