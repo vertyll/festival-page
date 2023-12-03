93216d88c9502773d07567feff247e32825a9641
Mikołaj Gawron <gawrmiko@gmail.com>
2023-12-03 21:37:21 +0100

    zmieniono kolor stopki

--- components/organism/Footer.jsx
+++ components/organism/Footer.jsx
@@ -2,7 +2,7 @@ import styled from "styled-components";
 import FooterInfoLinks from "../molecules/FooterInfoLinks";
 
 const StyledFooter = styled.footer`
-  background-color: var(--nav-color);
+  background-color: var(--footer-color);
   color: var(--light-text-color);
   margin: 0 auto;
   padding: 0 20px;
