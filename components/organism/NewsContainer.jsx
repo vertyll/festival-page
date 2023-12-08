73328f552e94f581fd1b23b87c9079b8d5b3b519
Mikołaj Gawron <gawrmiko@gmail.com>
2023-12-08 23:51:30 +0100

    dodano newsCount, gdy jest tylko jeden news, to wyświetla się on po środku

--- components/organism/NewsContainer.jsx
+++ components/organism/NewsContainer.jsx
@@ -3,23 +3,24 @@ import { RevealWrapper } from "next-reveal";
 import NewsBox from "./NewsBox";
 
 const NewsGrid = styled.div`
-  display: grid;
-  grid-template-columns: 1fr;
-  gap: 50px;
-  padding-top: 50px;
+    display: grid;
+    grid-template-columns: 1fr;
+    gap: 50px;
+    padding-top: 50px;
+    justify-items: center; /* Wyśrodkowanie pojedynczego elementu */
 
-  @media screen and (min-width: 985px) {
-    grid-template-columns: 1fr 1fr;
-  }
+    @media screen and (min-width: 985px) {
+        grid-template-columns: ${({ $newsCount }) => ($newsCount === 1 ? "1fr" : "1fr 1fr")};
+    }
 `;
 
 export default function NewsContainer({ news }) {
   return (
-    <NewsGrid>
+    <NewsGrid $newsCount={news?.length}>
       {news?.length > 0 &&
-        news.map((news, index) => (
-          <RevealWrapper key={news._id} delay={index * 50}>
-            <NewsBox key={news._id} {...news}></NewsBox>
+        news.map((newsItem, index) => (
+          <RevealWrapper key={newsItem._id} delay={index * 50}>
+            <NewsBox key={newsItem._id} {...newsItem}></NewsBox>
           </RevealWrapper>
         ))}
     </NewsGrid>
