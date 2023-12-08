7a038f6d772690d6794e9d8a43a79ae6987ab237
Mikołaj <75762608+vertyll@users.noreply.github.com>
2023-12-08 12:01:33 +0100

    Update SingleBox.jsx
    zmniejszono padding boxa

--- components/atoms/SingleBox.jsx
+++ components/atoms/SingleBox.jsx
@@ -3,7 +3,7 @@ import styled from "styled-components";
 const SingleBox = styled.div`
   background-color: var(--main-white-smoke-color);
   border-radius: 20px;
-  padding: 30px;
+  padding: 20px;
   display: flex;
   flex-direction: column;
   align-items: center;
