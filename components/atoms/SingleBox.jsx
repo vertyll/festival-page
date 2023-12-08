6f3c568a24a7ebcc7ddfc624e94c5a336bbd2a4c
Mikołaj Gawron <gawrmiko@gmail.com>
2023-12-08 15:54:45 +0100

    usunieto padding, kolor, shadow i radius

--- components/atoms/SingleBox.jsx
+++ components/atoms/SingleBox.jsx
@@ -1,14 +1,14 @@
 import styled from "styled-components";
 
 const SingleBox = styled.div`
-  background-color: var(--main-white-smoke-color);
-  border-radius: 20px;
-  padding: 30px;
+  //background-color: var(--main-white-smoke-color);
+  //border-radius: 20px;
+  //padding: 30px;
   display: flex;
   flex-direction: column;
   align-items: center;
   height: max-content;
-  box-shadow: var(--default-box-shadow);
+  //box-shadow: var(--default-box-shadow);
 `;
 
 export default SingleBox;
