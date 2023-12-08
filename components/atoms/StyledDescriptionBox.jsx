2759dc1def994c5563b5f0b523b745cc9e98a051
Mikołaj Gawron <gawrmiko@gmail.com>
2023-12-08 10:57:31 +0100

    poprawa stylowania

--- components/atoms/StyledDescriptionBox.jsx
+++ components/atoms/StyledDescriptionBox.jsx
@@ -3,6 +3,7 @@ import styled from "styled-components";
 const StyledDescriptionBox = styled.div`
   display: flex;
   max-width: 850px;
+  width: 100%;
   border-radius: 20px;
   background-color: var(--main-white-smoke-color);
   box-shadow: var(--default-box-shadow);
