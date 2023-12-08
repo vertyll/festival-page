dd2e267bd6d530d75f50df014fd419bcf1c6de82
Mikołaj Gawron <gawrmiko@gmail.com>
2023-12-08 23:00:23 +0100

    usuniecie niepotrzebnego display flex

--- components/atoms/ScrollToTop.jsx
+++ components/atoms/ScrollToTop.jsx
@@ -4,27 +4,26 @@ import styled from "styled-components";
 import IconTopArrow from "./IconTopArrow";
 
 const StyledButton = styled.div`
-  position: fixed;
-  right: 20px;
-  bottom: 20px;
-  cursor: pointer;
-  font-size: 30px;
-  color: var(--main-maize-color);
-  width: 30px;
-  height: 30px;
-  padding: 5px;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  background-color: var(--main-medium-slate-blue-color);
-  border-radius: 5px;
-  transition: 0.5s;
-  display: ${({ $show }) => ($show ? "flex" : "none")};
+    position: fixed;
+    right: 20px;
+    bottom: 20px;
+    cursor: pointer;
+    font-size: 30px;
+    color: var(--main-maize-color);
+    width: 30px;
+    height: 30px;
+    padding: 5px;
+    align-items: center;
+    justify-content: center;
+    background-color: var(--main-medium-slate-blue-color);
+    border-radius: 5px;
+    transition: 0.5s;
+    display: ${({$show}) => ($show ? "flex" : "none")};
 
-  &:hover {
-    color: var(--nav-color);
-    filter: brightness(0.85);
-  }
+    &:hover {
+        color: var(--nav-color);
+        filter: brightness(0.85);
+    }
 `;
 
 export default function ScrollToTop() {
