0abe694587c457c0b7bfd592566d93f51ef6b23e
Mikołaj Gawron <gawrmiko@gmail.com>
2026-02-07 20:14:07 +0100

    fix: fix env

--- next.config.js
+++ next.config.js
@@ -9,7 +9,7 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: process.env.S3_ENDPOINT || "",
+        hostname: process.env.S3_HOSTNAME || "localhost",
         port: "",
         pathname: "/**",
       },
